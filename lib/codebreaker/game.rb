5cc762f189c7b8cc55621ba3184e9915cb96fabf
wizard2007 <sponsor85@mail.ru>
2016-12-04 23:58:25 +0200

    restrict functionmalities
    [+] init_game
    
    [+] generate_secret_code
    
    [+] secret_code_valid

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -5,7 +5,19 @@ module Codebreaker
     end
 
     def start
-      @secret_code = '1234'
+      init_game
+    end
+
+    def init_game
+      @secret_code = generate_secret_code     
+    end
+    
+    def generate_secret_code 
+      '1234'
+    end
+    
+    def secret_code_valid?(code)
+      true
     end
   end
 end
