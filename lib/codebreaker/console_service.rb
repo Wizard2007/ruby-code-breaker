160738fd6b091ab493656c7620ca91fde4e95e9c
wizard2007 <sponsor85@mail.ru>
2016-12-15 00:44:26 +0200

    [*] simplify procedure pocess_user_input
    [+] property for detect game end and sessiob end

--- lib/codebreaker/console_service.rb
+++ lib/codebreaker/console_service.rb
@@ -1,10 +1,6 @@
-require "codebreaker/version"
-
-require 'codebreaker/game'
-
 module Codebreaker
-  class ConsoleService  
-    attr_accessor :is_game_over. :is_session_over
+  class ConsoleService
+    attr_accessor :is_game_over, :is_session_over
     def initializer()
       @is_game_over = false
       @is_session_over = false
@@ -14,18 +10,18 @@ module Codebreaker
       (user_input != '') ? process_user_input(user_input) : process_user_input(:empty_input)
     end
     def process_user_input(user_input)
-      user_input_sym = user_input.to_sym
-      respond_to_missing?(user_input_sym) ? send(user_input_sym) : send(:unknown_command, user_input)
+      #respond_to?(user_input) ? send(user_input) : send(:unknown_command, user_input.to_s)
+      send(user_input)
     end
     def empty_input
       puts 'your should enter comman or digits'
     end
     def unknown_command(command)
       puts "unknown command '#{command}'"
-    end
+    end   
     def exit
       @is_session_over = true
-      puts 'your exit game'      
+      puts 'your exit game'
     end
   end
 end
