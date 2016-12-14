3ad9da23eea77626b3b880e644f6810fc39c28b8
wizard2007 <sponsor85@mail.ru>
2016-12-14 03:26:52 +0200

    realise console

--- lib/codebreaker/console_service.rb
+++ lib/codebreaker/console_service.rb
@@ -3,7 +3,28 @@ require "codebreaker/version"
 require 'codebreaker/game'
 
 module Codebreaker
-  class ConsoleService
-
+  class ConsoleService  
+    def get_user_input
+      user_input = gets
+      unput.chmp
+      if 
+      process_user_input(user_input)
+    end
+    def process_user_input(user_input)
+      if respond_to_missing?(':#{user_input}') 
+        send(user_input.to_sym)
+      else
+        send(:unknown_command, user_input)
+      end 
+    end
+    def empty_input
+      puts 'your should enter comman or digits'
+    end
+    def unknown_command(command)
+      puts "unknown command '#{command}'"
+    end
+    def exit
+      puts 'your exit game'
+    end
   end
 end
--- spec/console_service_spec.rb
+++ spec/console_service_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 module Codebreaker
   RSpec.describe ConsoleService, 'Test ConsoleService' do
     subject { ConsoleService.new }
-    context '# Test Validate user input ' do
+    context '# Test user input ' do
+      it 'it should get user input' do 
+        subject.stub(:gets).and_return('test input')
+        subject.process_user_input
+        excpect(subject. )
+      end
       it 'it should be not empty '
-      it 'it shoul be valid console command'
-      it 'it shoul be validate by Game '
+      it 'it should be valid console command'
+      it 'it should be validate by Game '
     end
   end
 end
