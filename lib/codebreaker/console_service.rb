31610bde13506580cb190953a30299508726eac2
Eujine <sponsor85@mail.ru>
2016-12-14 22:51:27 +0200

    test console

--- lib/codebreaker/console_service.rb
+++ lib/codebreaker/console_service.rb
@@ -5,17 +5,12 @@ require 'codebreaker/game'
 module Codebreaker
   class ConsoleService  
     def get_user_input
-      user_input = gets
-      unput.chmp
-      if 
-      process_user_input(user_input)
+      user_input = gets.chmod!
+      (user_input != '') ? process_user_input(user_input) : process_user_input(:empty_input)
     end
     def process_user_input(user_input)
-      if respond_to_missing?(':#{user_input}') 
-        send(user_input.to_sym)
-      else
-        send(:unknown_command, user_input)
-      end 
+      user_input_sym = user_input.to_sym
+      respond_to_missing?(user_input_sym) ? send(user_input_sym) : send(:unknown_command, user_input)
     end
     def empty_input
       puts 'your should enter comman or digits'
